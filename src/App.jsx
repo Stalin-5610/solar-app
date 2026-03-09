2d5ca185a55075772e0b3df11ab7fc21c1bc1c98
Stalin <stalinyellu9@gmail.com>
2026-03-09 10:07:39 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+/** @format */
+
+import React, { useState } from "react";
+import planets from "./planetdata";
+import "./App.css";
+
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const nextPlanet = () => {
+    setIndex((index + 1) % planets.length);
+  };
+
+  const prevPlanet = () => {
+    setIndex((index - 1 + planets.length) % planets.length);
+  };
+
+  const planet = planets[index];
+
+  return (
+    <div className='container'>
+      <h1>🌌 Solar Planets App</h1>
+
+      <div className='viewer'>
+        <button onClick={prevPlanet} className='nav'>
+          ⬅️
+        </button>
+
+        <div className='planet-box'>
+          <img src={planet.image} alt={planet.name} />
+          <h2>{planet.name}</h2>
+          <p>{planet.info}</p>
+        </div>
+
+        <button onClick={nextPlanet} className='nav'>
+          ➡️
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/planetdata.js
@@ -0,0 +1,54 @@
+/** @format */
+
+const planets = [
+  {
+    name: "Mercury",
+    info: "Mercury is the closest planet to the Sun.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/18_mercury_new.png",
+  },
+  {
+    name: "Venus",
+    info: "Venus is the hottest planet in the Solar System.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/27_venus_jg.png",
+  },
+  {
+    name: "Earth",
+    info: "Earth is the only planet that supports life.",   
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/17_earth.png",
+  },
+  {
+    name: "Mars",
+    info: "Mars is called the Red Planet.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/19_mars.png",
+  },
+  {
+    name: "Jupiter",
+    info: "Jupiter is the largest planet in the Solar System.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/16_jupiter_new.png",
+  },
+  {
+    name: "Saturn",
+    info: "Saturn is famous for its rings.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/28_saturn.png",
+  },
+  {
+    name: "Uranus",
+    info: "Uranus rotates on its side.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/29_uranus.png",
+  },
+  {
+    name: "Neptune",
+    info: "Neptune is the farthest planet from the Sun.",
+    image:
+      "https://solarsystem.nasa.gov/system/feature_items/images/30_neptune.png",
+  },
+];
+
+export default planets;
